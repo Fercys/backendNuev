8b6885b0926b0c9b73be8a34482068aa25a6f14e
italo3 <italo@hostienda.com>
2020-01-23 13:52:03 -0400

    all_order endpoitn nuevo

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,6 +9,8 @@ use App\ContratoProducto;
 use App\Producto;
 use App\Detalle;
 use App\Encabezado;
+use App\Reserva;
+use App\Naviera;
 use Carbon\Carbon;
 
 class OrderController extends Controller
@@ -108,6 +110,21 @@ class OrderController extends Controller
             unset($contract_product); unset($product);
         }
         return response()->json(['Status' => 'Success', 'Value' => $response]);    
+    }
+    public function all_order(Request $request)
+    {   
+        $header = Encabezado::all();
+        $response = array();
+        foreach($header as $element){
+            $detail = Detalle::where('id_pedido', $element['id'])->first();
+            $product = Producto::where('id', $detail['id_producto'])->first();
+            $reserva = Reserva::where('id', $element['reserva'])->first();
+            $naviera = Naviera::where('id', $reserva['id_naviera'])->first(); 
+            $element_array_insert = (object) array('Encabezado'=>$element,'Detalle'=>$detail,'Producto'=>$product,'Naviera'=>$naviera);
+            array_push($response,$element_array_insert);
+            unset($contract_product); unset($product);
+        }
+        return response()->json(['Status' => 'Success', 'Value' => $response]);   
     }
     /*@italo: Actulizacion de Ordenes*/
     /* Estructura de ejemplo, ningun campo es obligatorio */
